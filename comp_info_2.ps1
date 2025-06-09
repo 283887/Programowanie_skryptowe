316a2cabc24c3dd63e01cce4841ca7999b4c9ba1
283887 <283887@student.pwr.edu.pl>
2025-06-09 13:34:39 +0200

    Add files via upload

---
+++ comp_info_2.ps1
@@ -0,0 +1,28 @@
+﻿# informacje o systemie
+
+$computer = $Env:COMPUTERNAME
+
+function akt_data{
+$data = Get-Date
+Write-Host "Aktualna data komputera $computer : $data" 
+}
+
+function wersja_systemu{
+$wersja = (Get-ComputerInfo).WindowsProductName
+Write-Host "Wersja systemu komputera $computer : $wersja" 
+}
+
+function nazwa{
+$user = $env:USERNAME
+Write-Host "Użytkownik komputera $computer : $user"
+}
+
+function ipadres{
+$adres = (Get-NetIPAddress| Where-Object{$_.InterfaceAlias -eq "Ethernet" -and $_.AddressFamily -eq "IPv4"}).IPAddress
+Write-Host "Adres Ip komputera $computer : $adres"
+}
+
+akt_data
+wersja_systemu
+nazwa
+ipadres
