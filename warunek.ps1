a7d7e95d8622936e116d77f63c2b81818fff7aaf
283887 <283887@student.pwr.edu.pl>
2025-06-09 13:38:17 +0200

    Add files via upload

---
+++ warunek.ps1
@@ -0,0 +1,19 @@
+﻿# Sprawdzenie liczby 
+
+[int]$liczba = Read-Host -Prompt "Podaj liczbę do sprawdzenia: "
+
+if($liczba -gt 10){
+
+Write-Host "Liczba jest większa od 10"
+
+}
+elseif($liczba -lt 10){
+
+Write-Host "Liczba jest mniejsza od 10"
+
+}
+else{
+
+Write-Host "Liczba jest równa 10"
+
+}
---
+++ zmienne.ps1
@@ -0,0 +1,8 @@
+﻿# Pole trójkąta
+
+[int]$wysokosc = Read-Host -Prompt "Podaj wysokość trójkąta: "
+[int]$podstawa = Read-Host -Prompt "Podaj podstawę trójkąta: "
+
+$pole = ($wysokosc*$podstawa)/2
+
+$pole
