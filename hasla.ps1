3974fe27fbc2c966b7e01e753fc65a0476a31c91
283887 <283887@student.pwr.edu.pl>
2025-06-09 13:33:56 +0200

    Add files via upload

---
+++ hasla.ps1
@@ -0,0 +1,13 @@
+﻿#Sprawdzanie hasła
+
+$wprowadzony_uzytkownik = Read-Host "Login"
+$wprowadzone_haslo = Read-Host "Podaj haslo:" Mask-Input 
+
+if($wprowadzony_uzytkownik -eq "Admin" -and $wprowadzone_haslo -eq "Password"){
+Write-Host "Logowanie udane!"
+}
+else
+
+{
+Write-Host "Niepoprawny login lub haslo"
+}
