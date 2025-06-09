ca8464d73b6f72b8ad7c3ca21b91545fe19c169f
283887 <283887@student.pwr.edu.pl>
2025-06-09 13:36:09 +0200

    Add files via upload

---
+++ pliki_csv.ps1
@@ -0,0 +1,23 @@
+﻿#pobieranie danych z pliku csv
+
+$sciezka = "Documents\zadanie6.csv"
+$dane = Import-Csv -Path $sciezka -Delimiter ";"
+
+#zbieramy wartosc z getcomputerinfo
+$wartosci = @{
+
+Computername = $env:COMPUTERNAME
+Manufacturer = (Get-ComputerInfo).CsManufacturer
+Model = (Get-ComputerInfo).CSModel
+SerialNumber = (Get-ComputerInfo).BiosSeralNumber
+CpuName = ((Get-ComputerInfo).CsProcessors).Name
+RAM = [math]::Round((Get-ComputerInfo).CsTotalPhysicalMemory / 1GB)
+
+}
+
+#interacja po danych z pliku
+foreach ($item in $dane) {
+    if ($item.GenerateReport -eq "True"){
+    Write-Output "$($item.Component) -> $($wartosci[$item.Component])"
+    }
+}
